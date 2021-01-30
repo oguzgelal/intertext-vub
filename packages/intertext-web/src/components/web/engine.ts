66d5d75cf51b453576323722405e15b391161dc0
Oguz Gelal <o.gelal77@gmail.com>
2021-01-30 00:11:28 -0500

    update packages

--- packages/intertext-web/src/components/web/engine.ts
+++ packages/intertext-web/src/components/web/engine.ts
@@ -1,4 +1,7 @@
 // import Engine from 'Engine/Engine'
-import Engine from '@intertext/engine2';
+import Engine from '@intertext/engine';
+
+// const newe = new Engine2()
+// console.log('========', newe)
 
 export default new Engine()
