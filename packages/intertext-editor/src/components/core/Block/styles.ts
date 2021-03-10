16c9da96cc97f71993bf26e769fdfe3efe1c8818
Oguz Gelal <o.gelal77@gmail.com>
2021-03-10 06:52:15 -0500

    block styles

--- packages/intertext-editor/src/components/core/Block/styles.ts
+++ packages/intertext-editor/src/components/core/Block/styles.ts
@@ -3,18 +3,34 @@ import { mode, getColor, transparentize } from "@chakra-ui/theme-tools"
 import { Dict } from "@chakra-ui/utils"
 
 const getBgColor = (props: Dict) => {
-  console.log('?????', props)
   const c = props.colorScheme;
-  console.log('ccccc', c)
   if (!c) return mode('white', 'black')(props)
-  return transparentize(getColor(props.theme, `${c}.300`), 0.8)(props.theme)
+  return transparentize(getColor(props.theme, `${c}.400`), 0.15)(props.theme)
+}
+
+const getBorderColor = (props: Dict) => {
+  const c = props.colorScheme;
+  if (!c) return mode('white', 'black')(props)
+  return `${c}.400`
 }
 
 export const InxBlock: ComponentMultiStyleConfig = {
   parts: ['base', 'container', 'pocketLeft', 'pocketRight'],
   baseStyle: props => ({
     base: {
-      bg: getBgColor(props)
+      p: 2,
+      bg: getBgColor(props),
+      borderRadius: 'lg',
+      borderWidth: 2,
+      borderColor: getBorderColor(props),
+      marginTop: 2,
+      marginBottom: 2,
+      '&:first-child': {
+        marginTop: 0,
+      },
+      '&:last-child': {
+        marginBottom: 0,
+      }
     },
     container: {
     },
